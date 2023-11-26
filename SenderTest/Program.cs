ac73e603377c3634f8c9e89cd2dbf714961445d7
YaslePoy <mmm606@inbox.ru>
2023-11-26 17:19:28 +0300

    single way listening

---
+++ SenderTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SenderTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
